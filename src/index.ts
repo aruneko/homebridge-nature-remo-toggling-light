82a36eebb8c7bfbfca0550d91a57be9cb1527774
Aruneko <aruneko99@gmail.com>
2022-04-25 11:09:46 +0900

    fix file name

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import { API } from 'homebridge'
 import { TogglingLightAccessory } from './accessory'
-import { ACCESSORY_IDENTIFIER, ACCESSORY_NAME } from './constsnts'
+import { ACCESSORY_IDENTIFIER, ACCESSORY_NAME } from './constants'
 
 export = (api: API) => {
   api.registerAccessory(ACCESSORY_IDENTIFIER, ACCESSORY_NAME, TogglingLightAccessory)
